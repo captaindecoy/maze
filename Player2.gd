bfc0370d86f4fc31eceefca39e6b6182fb353b17
Peter Maricle <pmaricle@gmail.com>
2023-10-08 15:57:37 -0700

    Initial commit, Godot testing

---
+++ Player2.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export var speed = 200
+@export var friction = 0.01
+@export var acceleration = 0.1
+
+func get_input():
+	var input = Vector2()
+	if Input.is_action_pressed('move_right'):
+		input.x += 1
+	if Input.is_action_pressed('move_left'):
+		input.x -= 1
+	if Input.is_action_pressed('move_down'):
+		input.y += 1
+	if Input.is_action_pressed('move_up'):
+		input.y -= 1
+	return input
+
+func _physics_process(delta):
+	var rs_look = Vector2(0,0)
+	var deadzone = 0.3
+	rs_look.y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+	rs_look.x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+	if rs_look.length() >= deadzone:
+		#rotation = rs_look.angle()
+		$AnimatedSprite2D.rotation = rs_look.angle()
+	#$AnimatedSprite2D.look_at(get_global_mouse_position())
+	#$AnimatedSprite2D.look_at(rightStick)
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
+	move_and_slide()
