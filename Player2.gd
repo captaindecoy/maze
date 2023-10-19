c322edc91ef63469700d9cd85d99dcce8382a7e2
Peter Maricle <pmaricle@gmail.com>
2023-10-18 21:19:58 -0700

    - Player can now fire a bullet that moves in the facing direction (right analog stick)

--- Player2.gd
+++ Player2.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var friction = 0.01
 @export var acceleration = 0.1
 
+var bullet_scene = preload("res://bullet.tscn")
+
 func get_input():
 	var input = Vector2()
 	if Input.is_action_pressed('move_right'):
@@ -17,13 +19,19 @@ func get_input():
 	return input
 
 func _physics_process(delta):
+	if(Input.is_action_just_pressed("shoot")):
+		var instance = bullet_scene.instantiate()
+		owner.add_child(instance)
+		#instance.rotation = rotation
+		#instance.transform.x = transform.x
+		instance.transform = transform
 	var rs_look : Vector2 = Vector2(0,0)
 	var deadzone = 0.3
 	rs_look.y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
 	rs_look.x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
 	if rs_look.length() >= deadzone:
-		#rotation = rs_look.angle()
-		$AnimatedSprite2D.rotation = rs_look.angle()
+		rotation = rs_look.angle()
+		#$AnimatedSprite2D.rotation = rs_look.angle()
 	#$AnimatedSprite2D.look_at(get_global_mouse_position())
 	#$AnimatedSprite2D.look_at(rightStick)
 	var direction = get_input()
