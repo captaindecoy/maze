00a5a156d020a39640d22027d53de67bb3f30607
Peter Maricle <pmaricle@gmail.com>
2023-11-02 22:03:29 -0700

    - Added baddie looking at and chasing player

--- scenes/baddie_01.gd
+++ scenes/baddie_01.gd
@@ -1,4 +1,13 @@
 extends CharacterBody2D
 
+@export var PlayerNodePath : NodePath
+@export var speed: int = 100
+
+func _physics_process(delta):	
+	var player = get_node(PlayerNodePath)
+	if player:
+		rotation = position.angle_to_point(player.position)
+		position += transform.x * speed * delta
+
 func die():
 	queue_free()
